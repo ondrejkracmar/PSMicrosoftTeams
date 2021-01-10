487b295726e4413d52cfbeac256d66aa09e7259d
Ondrej Kracmar <ondrej.kracmar@email.cz>
2021-01-10 18:20:21 +0100

    Annd new functionalities New-PSMTTeam

--- src/PSMicrosoftTeams/internal/functions/graphapi/Get-GraphApiUriPath.ps1
+++ src/PSMicrosoftTeams/internal/functions/graphapi/Get-GraphApiUriPath.ps1
@@ -4,14 +4,15 @@
     [CmdletBinding(DefaultParametersetName="Uri")]    
     param(
         [Parameter(ParameterSetName="ApiVersion", Mandatory=$false, Position=0)]
+        [ValidateSet('v1.0','beta')]
         [uri]$GraphApiVersion)
     
-    if($PSBoundParameters.ContainsKey('GraphApiVersion'))
+    if(Test-PSFParameterBinding -ParameterName  GraphApiVersion)
     {
-        return Join-UriPath -Uri (Get-PSFConfig -FullNamePSMicrosoftTeams.Settings.GraphApiUrl) -ChildPath $GraphApiVersion
+        return Join-UriPath -Uri (Get-PSFConfig -FullName PSMicrosoftTeams.Settings.GraphApiUrl) -ChildPath $GraphApiVersion
     }
     else 
     {
-        return Join-UriPath -Uri (Get-PSFConfig -FullNamePSMicrosoftTeams.Settings.GraphApiUrl) -ChildPath (Get-PSFConfig -FullName PSMicrosoftTeams.Settings.GraphApiVersion)
+        return Join-UriPath -Uri (Get-PSFConfig -FullName PSMicrosoftTeams.Settings.GraphApiUrl) -ChildPath (Get-PSFConfig -FullName PSMicrosoftTeams.Settings.GraphApiVersion)
     }
 }
