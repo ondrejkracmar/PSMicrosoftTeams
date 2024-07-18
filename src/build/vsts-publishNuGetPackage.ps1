77d2b1d4d2a3f3259c2115c1567e273a88b68097
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2024-07-18 12:26:31 +0000

    Fix issue

--- src/build/vsts-publishNuGetPackage.ps1
+++ src/build/vsts-publishNuGetPackage.ps1
@@ -38,8 +38,8 @@ $credential = New-Object System.Management.Automation.PSCredential ($FeedUsernam
 # Step 4
 # Upload NuGet Package
 if (-not ([string]::IsNullOrEmpty($PreRelease))) {
-    & $nugetPath push -Source $ArtifactFeedName -ApiKey ((New-Guid).Guid) "$($ModuleName).$($ModuleVersion).nupkg" -SkipDuplicate
-}
-else{
     & $nugetPath push -Source $ArtifactFeedName -ApiKey ((New-Guid).Guid) "$($ModuleName).$($ModuleVersion)-$($PreRelease).nupkg" -SkipDuplicate
+}
+else {
+    & $nugetPath push -Source $ArtifactFeedName -ApiKey ((New-Guid).Guid) "$($ModuleName).$($ModuleVersion).nupkg" -SkipDuplicate
 }
