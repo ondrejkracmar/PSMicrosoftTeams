045f5b283d2bd1d6034091245712e9a8fcb7211b
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2025-07-01 08:39:49 +0000

    Add new binanry library

---
+++ src/PSMicrosoftTeams/bin/assembly.ps1
@@ -0,0 +1,20 @@
+﻿try {
+    $alreadyLoaded = [AppDomain]::CurrentDomain.GetAssemblies() | Where-Object {
+        $_.GetName().Name -eq 'PSMicrosoftEntraID'
+    }
+    if (-not $alreadyLoaded) {
+        Add-Type -Path "$script:ModuleRoot\bin\PSMicrosoftEntraID.dll" -ErrorAction Stop
+    }
+    Add-Type -Path "$script:ModuleRoot\bin\PSMicrosoftTeams.dll" -ErrorAction Stop
+}
+catch {
+    Write-Warning "Failed to load PSMicrosoftEntraID Assembly! Unable to import module."
+    throw
+}
+try {
+    Update-TypeData -AppendPath "$script:ModuleRoot\types\PSMicrosoftTeams.ps1xml" -ErrorAction Stop
+}
+catch {
+    Write-Warning "Failed to load PSMicrosoftEntraID type extensions! Unable to import module."
+    throw
+}
