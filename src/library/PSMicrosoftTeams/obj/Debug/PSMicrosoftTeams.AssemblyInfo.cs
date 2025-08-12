acbb27db4fa4c450cb509852093a187c8c287cd3
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2025-08-12 14:23:49 +0200

    Change location PSMicrosoftEntraID.dll librarry to bin folder

--- src/library/PSMicrosoftTeams/obj/Debug/PSMicrosoftTeams.AssemblyInfo.cs
+++ src/library/PSMicrosoftTeams/obj/Debug/PSMicrosoftTeams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PSMicrosoftTeams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755c74af3fc2d0becdae6de0dea7d98375021cd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a462ff9a49c454039370064e6e68d105f23341d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PSMicrosoftTeams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PSMicrosoftTeams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
